8c2ac61d21ffc7cdc4dbd318afe14606ee8cae5b
beratukiqi <berat.u@live.se>
2023-03-09 19:16:50 +0100

    #9 Ta bort funktion klar

--- checkout.js
+++ checkout.js
@@ -22,6 +22,9 @@ async function fetchProducts() {
 
 
 async function addProductsToUI() {
+    let container = document.querySelector('.checkout-container__products')
+    container.innerHTML = "";
+
     let data = await fetchProducts()
 
     for (let i = 0; i < data.length; i++) {
@@ -34,6 +37,7 @@ async function addProductsToUI() {
                 let title = data[i].title;
                 let productContainer = document.createElement('article');
                 productContainer.classList.add('checkout-container__product-item')
+                productContainer.id = `${data[i].id}`
                 productContainer.innerHTML = `
                     <img src="${imgUrl}" alt="Bild på ${title} produkt">
                     <section class="prod-info">
@@ -43,85 +47,50 @@ async function addProductsToUI() {
                 `
                 container.appendChild(productContainer);
 
-
-                // let infoContainer = document.querySelector('.berat-test');
-                // let info = document.createElement('article');
-                // info.classList.add('checkout-info__product-info');
-                // info.classList.add(`${data[i].title}`);
-                
-
-                // info.innerHTML = `
-                //     <p>${data[i].title}</p>
-                //     <p>${matches}</p>
-                //     <p>${data[i].price}</p>
-                // ` 
-                // infoContainer.appendChild(info);
-
                 break;
             }
             
         }
         
     }
+
+    let deleteBtnList = document.querySelectorAll('.delete-btn')
+    deleteBtnList.forEach(btn => {
+        btn.addEventListener('click', () => {
+
+            let index = cartList.indexOf(btn.parentNode.parentNode.id);
+            cartList.splice(index, 1);
+            addProductsToUI()
+            addProductsInfoToUI()
+            updateProductCount()
+
+        })
+    })
 }
 
 
-addProductsToUI()
-
-// Kolla endast unik produkt en gång. Lägg till i lista.
-// En lista/array med endast EN PRODUKT av varje ID. 
-// Med den renderar vi ut endast "valda produkter".
-
-// for (let i = 1; i <= data.length; i++) {
-//     // Här börjar Ninja
-//     for (let j = 0; j < cartList.length; j++) {
-
-//         if (data[i] == cartList[j]) {
-//             let container = document.querySelector('.checkout-container__products')
-//             let imgUrl = product.url;
-//             let title = product.title;
-//             let productContainer = document.createElement('article');
-//             productContainer.classList.add('checkout-container__product-item')
-//             productContainer.innerHTML = `
-//                 <img src="${imgUrl}" alt="Bild på ${title} produkt">
-//                 <section class="prod-info">
-//                     <h3>${title}</h3>
-//                     <button class="delete-btn">Ta bort från varukorg</button>
-//                 </section>
-//             `
-//             container.appendChild(productContainer);
-//             break;
-//         }
-        
-//     }
-    
-// }
-
-
-
-
-
 async function addProductsInfoToUI() {
+    let infoContainer = document.querySelector('.berat-test');
+    infoContainer.innerHTML = "";
     let data = await fetchProducts()
     // Product info
     
     data.forEach(product => {
-        let count = 0;
-
+        
         for (let i = 0; i < cartList.length; i++) {
-
+            
             if (product.id == cartList[i]) {
                 let infoContainer = document.querySelector('.berat-test');
                 // infoContainer.innerHTML = "";
                 let info = document.createElement('article');
                 info.classList.add('checkout-info__product-info');
-                info.id =product.id;
+                info.id = product.title;
                 
-
+                
                 info.innerHTML = `
-                    <p class="product-name">${product.title}</p>
-                    <p class="product-amount">${product.quantity}</p>
-                    <p class="product-price">${product.price}</p>
+                <p class="product-name">${product.title}</p>
+                <p class="product-amount">${product.quantity}</p>
+                <p class="product-price">${product.price}</p>
                 ` 
                 infoContainer.appendChild(info);
                 break;
@@ -130,13 +99,6 @@ async function addProductsInfoToUI() {
         }
     })
 }
-
-// För varje produkt skapar vi ett count värde.
-// Kolla igenom våran Cart lista.
-// För varje träff, öka produktens count.
-// Uppdatera Count till UI.
-// När hela carten är slut, kör nästa produkt.
-
 
 async function updateProductCount() {
     let data = await fetchProducts()
@@ -152,24 +114,22 @@ async function updateProductCount() {
         
         if (document.getElementById(`${product.id}`)) {
             console.log(`${product.title} har ${count} st`);
-
-            let target = document.getElementById(`${product.id}`)
+            
+            let target = document.getElementById(`${product.title}`)
             console.log(target,' TARGET');
             target.innerHTML = `
-                    <p class="product-name">${product.title}</p>
-                    <p class="product-amount">${count}</p>
-                    <p class="product-price">${product.price * count}</p>
-                ` 
-
+            <p class="product-name">${product.title}</p>
+            <p class="product-amount">${count}</p>
+            <p class="product-price">${product.price * count}</p>
+            ` 
+            
         }
-        // target.childNodes;
-        // console.log(target2,' TARGET2222222 HERE');
-        // let amount = target.nth-child(2);
-        
-        // amount.innerHTML = count;
     })
 }
 
 
+
+
+addProductsToUI()
 addProductsInfoToUI()
 updateProductCount()
